63cb86a5e4fe7c4daf527aa6ecffb7d1cff9ac1a
Diego La Madrid <diegolamadrid92@gmail.com>
2017-05-05 13:48:29 +0000

    d

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+<?php include 'db.php';?>
 <html>
 <head>
 	<title>PHP Starter Application</title>
@@ -6,16 +7,50 @@
 	<link rel="stylesheet" href="style.css" />
 </head>
 <body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
+
+<img class = 'newappIcon' src='images/arriba.png' style='width: 66%;''>
+
+
+<br>
+
+<div>
+<h2 style='margin-left: 18%;'>For data scientists</h2>	
+<center><table  style='width: 66%; padding-top: 0 !important;'>
+
+<?php
+
+ $sql = "SELECT * from servicios";
+  $result = $conn->query($sql);
+  if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+      	
+      
+
+      	?>
+      <tr>
+       
+      <td style='width: 10%;'> <?php echo "<img class = 'newappIcon' src='".$row['imagen_servicio']."'> "    ?> </td>
+      <td><h1 id = "message"><?php echo $row['nombre_servicio']?></h1>
+      <p class='description'></p><?php echo $row['descripcion_servicio']?></td>
+
+      </tr>
+     <?php 
+    
+     }
+  } else {
+      echo "0 results";
+  }
+  $conn->close();
+ ?> 
+
+
+	</table></center>
+	
+	</div>
+	<br>
+
+<img class = 'newappIcon' src='images/abajo.png'>
+	
 </body>
 </html>
